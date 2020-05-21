5b22522b5ad29736f67260128c59999dcc767a5a
Aleksandr <sanekclubstr@mail.ru>
2020-05-22 04:11:18 +0500

    Fix wtforms_strip bug

--- forms/login_form.py
+++ forms/login_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 from wtforms.validators import DataRequired
-from wtforms_strip import StrippedPasswordField, StrippedStringField
+from .wtforms_strip import StrippedPasswordField, StrippedStringField
 
 
 class LoginForm(FlaskForm):
