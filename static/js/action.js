50da2238fcbcaae14565c9a2c5d24a2c1b622ea8
Aleksandr <sanekclubstr@mail.ru>
2020-05-25 04:40:32 +0500

    Fix

--- static/js/action.js
+++ static/js/action.js
@@ -3,6 +3,7 @@ window.addEventListener("orientationchange", fixBackgroundLayout, false);
 window.addEventListener("load", fixBackgroundLayout, false)
 
 function fixBackgroundLayout() {
+    $('#background').css({'height': '100%'});
     var h = $('#background').height();
     $('#background').css({'height': h + 'px'});
 }
