fa9a8d517ee00b875aabf9cea8b3e8b6e5f7dacf
Aleksandr <sanekclubstr@mail.ru>
2020-05-25 22:56:38 +0500

    Add menu

--- static/js/action.js
+++ static/js/action.js
@@ -7,3 +7,14 @@ function fixBackgroundLayout() {
     var h = $('#background').height();
     $('#background').css({'height': h + 'px'});
 }
+
+$('body').on('click', '.control-eye', function(){
+    if ($(this).hasClass('view')) {
+        $(this).removeClass('view');
+        $(this).parent().children('input').attr('type', 'text');
+    } else {
+        $(this).addClass('view');
+        $(this).parent().children('input').attr('type', 'password');
+    }
+	return false;
+});
